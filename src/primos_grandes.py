171f58f5a468a171f415fc56ebf38de72d925da0
Igor Junqueira <igor.junqueira@nubank.com.br>
2022-10-14 18:43:10 -0300

    primos grandes python

--- src/primos_grandes.py
+++ src/primos_grandes.py
@@ -1,18 +1,21 @@
-from random import random
+import random
 from crivos import crivo_de_eratostenes
 
 class BigPrime:
-    def __init__(self, bitSize):
+    def __init__(self, bitSize, primesUntil=1000, trials=20):
         self.bitSize = bitSize
+        while True:
+            self.valor = self.nBitRandom(self.bitSize)
+            if self.lowLevelPrime(self.valor, primesUntil):
+                if self.isMillerRabin(self.valor, trials): break
 
-    def nBitRandom(bitSize):
+    def nBitRandom(self, bitSize):
         """
         Dado N bits, retorna um número aleatório de N bits
         """
         return(random.randrange(2**(bitSize-1)+1, 2**bitSize-1))
 
-    
-    def lowLevelPrime(prime_candidate, primesUntil=1000):
+    def lowLevelPrime(self, prime_candidate, primesUntil=1000):
         """"
         Dado um candidato a primo, retorna booleano indicando primalidade
         de baixo nível (com primos até 'primesUntil')
@@ -24,4 +27,25 @@ class BigPrime:
             if not (prime_candidate % divisor): return False
         return True
 
-    
+    def trialComposite(self, prime_candidate, round_tester,
+                       evenComponent, maxDivisionsByTwo):
+        if pow(round_tester, evenComponent, prime_candidate) == 1: return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * evenComponent,
+                   prime_candidate)== prime_candidate-1: return False
+        return True 
+
+    def isMillerRabin(self, prime_candidate, trials=20):
+        maxDivisionsByTwo = 0
+        evenComponent = prime_candidate-1
+        while evenComponent % 2 == 0:
+            evenComponent >>= 1
+            maxDivisionsByTwo += 1
+        assert(2**maxDivisionsByTwo * evenComponent == prime_candidate-1)
+        for _ in range(trials):
+            round_tester = random.randrange(2, prime_candidate)
+            if self.trialComposite(prime_candidate, round_tester,
+                evenComponent, maxDivisionsByTwo): return False
+        return True
+
+    def __str__(self): return str(self.valor)\
